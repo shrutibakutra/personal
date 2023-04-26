f00304bcd58684fc076885bade0a58a844537629
shrutibakutra <shruti15bakutra@gmail.com>
2023-04-26 16:00:33 +0530

    update app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { MainNav } from './Navigation/nav';
+import { AboutMe } from './AboutMe/aboutme';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { Work } from './Work/work';
+
+function App() {
+  return (
+    <div className='App'>
+      <MainNav />
+      <Router>
+        <Routes>
+          <Route path="/" element={<AboutMe />} />
+          <Route path="/work" element={<Work />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
