bdfa8e31662004d9987265a1d57662ca002c94de
shrutibakutra <shruti15bakutra@gmail.com>
2023-04-27 18:35:15 +0530

    update routes

--- src/App.js
+++ src/App.js
@@ -3,22 +3,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { MainNav } from './Navigation/nav';
 import { AboutMe } from './AboutMe/aboutme';
 import {
-  BrowserRouter as Router,
+  BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
 import { Work } from './Work/work';
 
+
 function App() {
   return (
     <div className='App'>
-      <MainNav />
+      {/* <MainNav /> */}
+      {/* 
       <Router>
-        <Routes>
+        <Routes basename='/'>
           <Route path="/" element={<AboutMe />} />
           <Route path="/work" element={<Work />} />
         </Routes>
-      </Router>
+      </Router> */}
+
+
+
+      <BrowserRouter basename="/">
+        <MainNav />
+        <div>
+          <Routes>
+            <Route path="/" element={<AboutMe />} />
+            <Route exact path="/work" element={<Work />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
