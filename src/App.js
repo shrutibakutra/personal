0d39e4a3c8f1ba5471d189319c9831d84e29b972
shrutibakutra <shruti15bakutra@gmail.com>
2023-04-28 14:15:38 +0530

    nav update

--- src/App.js
+++ src/App.js
@@ -3,36 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { MainNav } from './Navigation/nav';
 import { AboutMe } from './AboutMe/aboutme';
 import {
-  BrowserRouter,
   Routes,
   Route,
+  HashRouter,
 } from "react-router-dom";
 import { Work } from './Work/work';
-
 
 function App() {
   return (
     <div className='App'>
-      {/* <MainNav /> */}
-      {/* 
-      <Router>
-        <Routes basename='/'>
-          <Route path="/" element={<AboutMe />} />
-          <Route path="/work" element={<Work />} />
-        </Routes>
-      </Router> */}
-
-
-
-      <BrowserRouter basename="/">
+      <HashRouter>
         <MainNav />
-        <div>
-          <Routes>
-            <Route path="/" element={<AboutMe />} />
-            <Route exact path="/work" element={<Work />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AboutMe />} />
+          <Route exact path="/work" element={<Work />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
