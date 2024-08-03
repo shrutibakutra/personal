7aa0f6bd85e7ef778ea90f657f09426274679565
shrutibakutra <shruti15bakutra@gmail.com>
2024-08-03 17:46:39 +0200

    fix mail redirectiom

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
     <App />
     <footer className='footer'>
       <div className='footer__content'>
-        <div> <FontAwesomeIcon icon={faEnvelope} /> <a href="mailto:shruti15bakutra@gmail.com" target="_blank">shruti15bakutra@gmail.com</a></div>
+        <div> <FontAwesomeIcon icon={faEnvelope} /> <a href="mailto:shruti15bakutra@gmail.com">shruti15bakutra@gmail.com</a></div>
         <div><FontAwesomeIcon icon={faHouse} /> Challex, France</div>
         <div><i class="fab fa-linkedin-in"></i><a href='https://www.linkedin.com/in/shruti-bakutra-037829156/'> https://www.linkedin.com/in/shruti-bakutra-037829156/</a></div>
       </div>
