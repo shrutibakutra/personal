5d116324cd7f9e243f1cf81764aac081b76a71f5
shrutibakutra <shruti15bakutra@gmail.com>
2024-08-05 16:43:19 +0200

    updated files

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { MainNav } from './Navigation/nav';
 import { AboutMe } from './AboutMe/aboutme';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
 import { Work } from './Work/work';
+import RedirectToHome from './RedirectToHome';
 
 function App() {
   return (
@@ -15,8 +16,9 @@ function App() {
       <MainNav />
       <Router>
         <Routes>
-          <Route path="/" element={<AboutMe />} /> 
+          <Route path="/" element={<AboutMe />} />
           <Route path="/work" element={<Work />} />
+          <Route path="*" element={<RedirectToHome />} />
         </Routes>
       </Router>
     </div>
